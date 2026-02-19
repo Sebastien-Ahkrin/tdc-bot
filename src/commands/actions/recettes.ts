c05bdd9c5d824dac1c9f517c75d1f8fb78b0ac40
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-02-19 23:40:14 +0100

    feat: use ky to get recipes

--- src/commands/actions/recettes.ts
+++ src/commands/actions/recettes.ts
@@ -1,37 +1,50 @@
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import ky from 'ky';
 
 const command = new SlashCommandBuilder()
   .setName('recettes')
   .setDescription('Affiche toutes les recettes');
 
+interface Recipe {
+  id: number;
+  name: string;
+  duration: number;
+  link: string;
+  difficulty: string | null;
+  type: string | null;
+  notice: string | null;
+}
+
 async function execute(interaction: any) {
+  const recipe = await ky.get<Recipe>('http://localhost:3333/recipes').json();
+
   const embed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle('🎲 Ta recette aléatoire 😍')
     .setFields([
       {
         name: 'Titre',
-        value: 'placeholder',
+        value: recipe.name,
       },
       {
         name: 'Temps',
-        value: 'placeholder',
+        value: String(recipe.duration),
       },
       {
         name: 'Difficulé',
-        value: 'placeholder',
+        value: recipe.difficulty ?? '',
       },
       {
         name: 'Type',
-        value: 'placeholder',
+        value: recipe.type ?? '',
       },
       {
         name: 'Lien',
-        value: 'placeholder',
+        value: recipe.link,
       },
       {
         name: 'Conseil',
-        value: 'placeholder',
+        value: recipe.notice ?? '',
       },
     ]);
 
